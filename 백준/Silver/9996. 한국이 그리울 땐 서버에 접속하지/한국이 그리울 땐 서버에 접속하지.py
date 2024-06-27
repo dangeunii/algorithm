04d2d1c1d99dd90cc0e2dae46df0996fcb42d6ec
dangeunii <101170415+dangeunii@users.noreply.github.com>
2024-06-28 00:14:50 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.py
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.py
@@ -5,29 +5,31 @@ import sys
 n = int(input())
 pattern = input()
 isFront = 0
-front = []
-back = []
 
-for s in pattern:
-    if s != "*":
-        if isFront == 0:
-            front.append(s)
-        else:
-            back.append(s)
-    else:
-        isFront = 1
+pos = 0
+for i in range(len(pattern)):
+    if pattern[i] == "*":
+        pos = i
 
-front_s = "".join(s for s in front)
-back_s = "".join(s for s in back)
+front_s = pattern[0:pos:]
+back_s = pattern[pos+1::]
+
+len_s = len(front_s)
+len_b = len(back_s)
+# front_s = "".join(s for s in front)
+# back_s = "".join(s for s in back)
+
 for i in range(n):
     ss = input()
-    len_s = len(front)
-    len_b = len(back)
+
+    # 반례 처리를 위해서
     if len(ss) < len_s+len_b:
         print("NE")
         continue
+
     ss_s = ss[0:len_s:]
     ss_b = ss[len(ss)-len_b::]
+
     if ss_s == front_s:
         if ss_b == back_s:
             print("DA")
