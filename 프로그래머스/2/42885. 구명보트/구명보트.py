0b5760db811d3895774867876af2f9979793b084
dangeunii <101170415+dangeunii@users.noreply.github.com>
2024-08-09 22:04:53 +0900

    [level 2] Title: 구명보트, Time: 8.80 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,13 @@
+from collections import deque
+def solution(people, limit):
+    people.sort()
+    answer = 0
+    p1 = 0
+    p2 = len(people) -1
+    
+    while(p1 <= p2):
+        if people[p1] + people[p2] <= limit:
+            p1 += 1
+        p2 -= 1
+        answer += 1
+    return answer
