fd9fedec8f1a107c195d619b77ac2bc6bc10816f
dangeunii <101170415+dangeunii@users.noreply.github.com>
2025-02-03 19:44:49 +0900

    [Silver I] Title: 후보 추천하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1713. 후보 추천하기/후보 추천하기.cc
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.cc
@@ -1,88 +1,90 @@
 #include<iostream>
+#include<deque>
 #include<algorithm>
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    // freopen("input.txt", "r", stdin);
+   //freopen("input.txt", "r", stdin);
 
-    int N, M;
-    int arr[101] = {0}; // 정렬할 학생 배열
-    int time[101] = {0}; // 학생이 처음 추천받은 시간
-    int reco[101] = {0}; // 학생이 추천받은 횟수
+	int N, M;
+	int arr[101];
+	int time[101] = {0};
+	int reco[101] = {0};
 
-    cin >> N >> M;
+	cin >> N >> M;
 
-    int cnt = 0;  // 게시판에 등록된 학생 수
+	int cnt = 0;
 
-    for (int i = 0; i < M; i++) {
-        int x;
-        cin >> x;
+	for (int i = 0; i < M; i++) {
+		int x;
+		cin >> x;
 
-        // 이미 게시판에 있는 경우 추천 수 증가
-        if (reco[x] > 0) {
-            reco[x]++;
-        }
-        else {
-            if (cnt < N) {  // 게시판에 자리 남아있을 경우
-                reco[x] = 1;
-                time[x] = i+1;  // 🔹 1-based index 유지
-                cnt++;
-            }
-            else {  // 게시판이 가득 찼을 경우
+		//cout << "x : " << x << "\n";
+		//이미 추천 받은 경우
+		if (reco[x] > 0) {
+			reco[x]++;
+		}
+		// 추천 받은 적이 없다면 일단 틀이 비었나 확인
+		else {
+			if (cnt < N) {
+				reco[x] = 1;
+				time[x] = i+1;
+				cnt++;
+				//cout << "cnt : " << cnt << "\n";
+			}
+			// 틀이 안비었어 -> 먼저 추천수 확인하기
+			else {
+				// 일단 최소 추천수를 갖는 학생 찾기
                 int ridStudent = -1;
                 int comtime = 1001;
                 int comreco = 1001;
 
-                // 1️⃣ 최소 추천 수 찾기
-                for (int j = 1; j < 101; j++) {
-                    if (reco[j] > 0 && reco[j] < comreco) {
+                for(int j = 1 ; j < 101; j++){
+                    if(reco[j] != 0 && comreco > reco[j]){
                         comreco = reco[j];
                     }
                 }
 
-                // 2️⃣ 최소 추천 수를 가진 학생 중 가장 오래된 학생 찾기
-                for (int j = 1; j < 101; j++) {
-                    if (reco[j] == comreco) {
-                        if (time[j] < comtime) {
+                for(int j = 1; j < 101; j++){
+                    // 가장 오래된, time이 작은애를 ridStudent로 지정
+                    if (reco[j] == comreco)
+                    {
+                        if(comtime > time[j]){
                             comtime = time[j];
                             ridStudent = j;
                         }
                     }
                 }
 
-                // 3️⃣ 해당 학생 삭제 (유효한 경우만)
-                if (ridStudent != -1) {
-                    reco[ridStudent] = 0;
-                    time[ridStudent] = 0;
-                }
+                //cout << "ridStudent : " << ridStudent << " reco : " << reco[ridStudent] << " time : " << time[ridStudent] << "\n";  
 
-                // 4️⃣ 새로운 학생 추가
+                //찾은 ridStudent를 기준으로 삭제 및 새로운 학생 넣기
+                reco[ridStudent] = 0;
+                time[ridStudent] = 0;
+
                 reco[x] = 1;
                 time[x] = i+1;
-            }
+			}
+		}
+	}
+
+	int k = 0;
+	for (int i = 1; i < 101; i++) {
+		if(reco[i]>0){
+            arr[k] = i;
+            k++;
         }
-    }
+	}
 
-    // 5️⃣ 추천된 학생 목록을 배열에 저장
-    int k = 0;
-    for (int i = 1; i < 101; i++) {
-        if (reco[i] > 0) { // 추천 수가 1 이상인 학생만 포함
-            arr[k++] = i;
-        }
-    }
+	
+	sort(arr,arr+k);
 
-    // 6️⃣ 학생 번호 정렬
-    sort(arr, arr + k);
-
-    // 7️⃣ 정렬된 학생 번호 출력
-    for (int i = 0; i < k; i++) {
-        cout << arr[i] << " ";
-    }
-
-    return 0;
+	for (int i = 0; i < k; i++) {
+		cout << arr[i] << " ";
+	}
 }
