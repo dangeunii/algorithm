79cb37f5483583c96496e07c0e466ca54be6f9e6
dangeunii <101170415+dangeunii@users.noreply.github.com>
2025-02-04 11:36:41 +0900

    [Gold IV] Title: 가르침, Time: 128 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.cc
+++ 백준/Gold/1062. 가르침/가르침.cc
@@ -6,56 +6,37 @@
 using namespace std;
 
 int k, N;
+int check[26];
 int cnt = 0;
 vector<char> compare;
 int valid = 0;
-int arr[51][26];
+int arr[51];
 int ans = 0;
 int checkSum = 0;
 
-vector<char> vec = {'b', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'o', 'p', 'q', 'r', 's', 'u', 'w', 'v', 'x', 'y', 'z'};
+vector<char> vec = {'b', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'o', 'p', 'q', 'r', 's', 'u', 'v', 'w', 'x', 'y', 'z'};
     
 
-void combi(int start , vector<char> b){
+void combi(int start , vector<char> b, int checkSum){
     //기저 조건
     if (b.size() == k-5){
         int can = 0;
-        int check[26] = {0};
-        char n[5] = {'a','n','t','c','i'};
-        for(char c : n){
-            int idx = (c -'a');
-            check[idx] = 1;
-        }
-        //cout << "\n";
         // 문제에 나온 로직 추가
-        for(char  c: b){
-            //cout << c << "\n";
-            int idx = (c - 'a');
-            check[idx] = 1;
-        }
-        /*
-
-        for(int i = 0 ; i < 26; i++){
-            cout << check[i] << " ";
-        }
-        cout << "\n";
-*/
-        for(int i = 0; i < N; i++){
-            int flag = 1;
-            for(int j = 0 ; j < 26; j++){
-                if(arr[i][j] == 1 && check[j] == 0){
-                    flag = 0;
-                }
+        for (int i = 0; i < N; i++){
+            //cout << " arr : " << arr[i] <<"\n";
+            //cout << checkSum << "\n";
+            if((arr[i] & checkSum) == arr[i]){
+                can++;
+                //cout << "can++ :" << can <<"\n";
             }
-            if (flag == 1) can++;
-            //cout<< "can : " << can << "\n";
         }
         ans = max(ans, can);
+        return;
     }
 
     for(int i  = start+1; i < vec.size(); i ++){
         b.push_back(vec[i]);
-        combi(i,b);
+        combi(i,b, checkSum | (1 << (vec[i]-'a')));
         b.pop_back(); 
     }
     return;
@@ -70,39 +51,40 @@ int main(){
     vector<char> b;
 
     cin >> N >> k;
+
+    char n[5] = {'a','n','t','c','i'};
+    for(char c : n){
+        checkSum |= 1 << (c - 'a');
+    }
+    //cout << checkSum;
  
     if(k < 5){
         cout << cnt;
     }
     else{
         for (int i = 0; i < N; i++){
-            char n[5] = {'a','n','t','c','i'};
-            for(char c : n){
-                int idx = c -'a';
-                arr[i][idx] = 1;
-            }
             string s;
             compare.clear();
             cin >> s;
             int num = 0;
             //cout << "s : " << s << "\n";
             for(char c : s){
-                int idx = c - 'a';
-                arr[i][idx] = 1;
+                // 비트 마스킹
+                num |= 1 << (c- 'a'); 
+                /*
+                if(c != 'a' && c != 'n' && c != 't' && c != 'c' && c != 'i'){
+                    compare.push_back(c);
+                }*/
             }
-            
+            arr[i] = num;
+            /*
+            for(char c : compare){
+                cout << c << " ";
+            }*/
+            //cout << "\n";
 
         }
-
-        /*
-        for(int i = 0; i < N ; i++){
-            for(int j = 0; j < 26; j++){
-                cout << arr[i][j] << " ";
-            }
-            cout << "\n";
-        }
-        */
-        combi(-1, b);
+        combi(-1, b, checkSum);
         cout << ans;
     }
     
